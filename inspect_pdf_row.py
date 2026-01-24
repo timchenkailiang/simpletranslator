0ed29b4d19be0b6cfdbf7b55648438eda825434b
Kailiang Chen <tim@Kailiangs-MBP.attlocal.net>
2026-01-23 19:06:06 -0600

    Cleanup: Remove debugging scripts, legacy extractor, and temporary test files

--- inspect_pdf_row.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("/Users/tim/Documents/dad/simpletranslator/source/PO 35306 - Jeffco.pdf") as pdf:
-    for page in pdf.pages:
-        text = page.extract_text()
-        for line in text.split('\n'):
-            if "3600" in line:
-                print(f"Line found: '{line}'")
